8e7d39849a241f9112bbde5a6e8ee630deb04922
Alicia Angel <aliciaangel@Alicias-MacBook-Pro.local>
2022-09-08 09:25:56 +0800

    Add read US file (unfinished)

--- code/notes/functions.py
+++ code/notes/functions.py
@@ -1,5 +1,58 @@
 # read files + data cleaning
+def read_UsFile(filename):
+  income_year_dict = {}
+  with open(filename,'r') as data:
+    content = data.read()
+    lines = content.splitlines()
+    for line in lines:
+      #Add the all people data to the dictionary
+      if "All Races" in line:
+        income_year_dict["All"] = (lines[lines.index(line)+3].split("\t")[1:-6])
+      
+      #Find the start index and end index of age data
+      if "Age" in line:
+        age_start = lines.index(line)+3
+      if "Mean age" in line:
+        age_end = lines.index(line)
 
+    #Check if each age category is inside the file and add it to the dictionary
+    for line in lines[age_start:age_end]:
+      if "15 to 24" in line:
+        income_year_dict["15_24"] = line.split("\t")[1:-6]
+      elif "25 to 34" in line:
+        income_year_dict["25_34"] = line.split("\t")[1:-6]
+      elif "35 to 44" in line:
+        income_year_dict["35_44"] = line.split("\t")[1:-6]
+      elif "45 to 54" in line:
+        income_year_dict["45_54"] = line.split("\t")[1:-6]
+      elif "55 to 64" in line:
+        income_year_dict["55_64"] = line.split("\t")[1:-6]
+      elif "65 to 74" in line:
+        income_year_dict["65_74"] = line.split("\t")[1:-6]
+      elif "75 years and over" in line:
+        income_year_dict["75+"] = line.split("\t")[1:-6]
+    return income_year_dict
+    
+
+def read_UsData(filename_M, filename_F):
+  final_dict, final_dict_M, final_dict_F = {}, {}, {}
+
+  male_data = read_UsFile(filename_M)
+  female_data = read_UsFile(filename_F)
+
+  #for i in range(len(male_data)):
+  for age_group, number in male_data.items():
+    final_dict_M["M"] = number
+    final_dict_F["F"] = female_data[age_group]
+    final_dict[age_group] = final_dict_M, final_dict_F
+
+
+  return final_dict
+
+filename_F = "data/us/pinc_female.tsv"
+filename_M = "data/us/pinc_male.tsv"
+
+print(read_UsData(filename_M,filename_F))
 # calculate gini coefficient
 
 # plot lorenz curve
