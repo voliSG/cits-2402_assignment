f4d3dda4d6f7caea059598edc586a9e290546890
unknown <23225186@student.uwa.edu.au>
2022-09-08 22:50:43 +0800

    add pie chart for aus

--- code/notes/sex_pie_chart.py
+++ code/notes/sex_pie_chart.py
@@ -15,3 +15,32 @@ plt.pie(total_US_list, labels=total_label)
 plt.title("Gender proportion in US")
 
 plt.show()
+
+# Australia
+# getiing data for aus
+data_AUS = read_AusData(path_dataFolder ,filenames_aus)
+# generating size
+size_sex=[]
+# initializing total in male and female
+total_m=0
+total_f=0
+count=0
+# adding up all numbers in all age group for male and female
+for key, value in data_AUS.items():
+    if count != 7:
+        for k , v in value.items():
+            # male
+            if k == 'M':
+                for i in range(len(v)):
+                    total_m += v[i]
+            # female
+            if k == 'F':
+                for i in range(len(v)) :
+                    total_f += v[i]   
+        count+=1
+size_sex.append(total_m)
+size_sex.append(total_f)
+
+# plotting pie chart for sex in australia
+plt.pie(size_sex,labels=['M', 'F'], rotatelabels=True)
+plt.show()
