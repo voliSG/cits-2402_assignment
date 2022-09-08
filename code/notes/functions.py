6f5bff0e464d26208da3c16931ffa7a99949bbf4
Sean <2003seangarcia@gmail.com>
2022-09-08 15:53:53 +0800

    type cast str data to int

--- code/notes/functions.py
+++ code/notes/functions.py
@@ -7,7 +7,12 @@ def read_UsFile(filename):
         for line in lines:
             #Add the all people data to the dictionary
             if "All Races" in line:
-                income_year_dict["All"] = (lines[lines.index(line)+3].split("\t")[1:-6])
+                income_people = (lines[lines.index(line)+3].split("\t")[1:-6])
+                
+                # replace comma and typecast strings to ints
+                income_people_int = [int(num.replace(",", "")) for num in income_people]
+                
+                income_year_dict["All"] = income_people_int
       
             #Find the start index and end index of age data
             if "Age" in line:
@@ -17,20 +22,25 @@ def read_UsFile(filename):
 
         #Check if each age category is inside the file and add it to the dictionary
         for line in lines[age_start:age_end]:
+            income_people = line.split("\t")[1:-6]
+            
+            # replace comma and typecast strings to ints
+            income_people_int = [int(num.replace(",", "")) for num in income_people]
+
             if "15 to 24" in line:
-                income_year_dict["15_24"] = line.split("\t")[1:-6]
+                income_year_dict["15_24"] = income_people_int
             elif "25 to 34" in line:
-                income_year_dict["25_34"] = line.split("\t")[1:-6]
+                income_year_dict["25_34"] = income_people_int
             elif "35 to 44" in line:
-                income_year_dict["35_44"] = line.split("\t")[1:-6]
+                income_year_dict["35_44"] = income_people_int
             elif "45 to 54" in line:
-                income_year_dict["45_54"] = line.split("\t")[1:-6]
+                income_year_dict["45_54"] = income_people_int
             elif "55 to 64" in line:
-                income_year_dict["55_64"] = line.split("\t")[1:-6]
+                income_year_dict["55_64"] = income_people_int
             elif "65 to 74" in line:
-                income_year_dict["65_74"] = line.split("\t")[1:-6]
+                income_year_dict["65_74"] = income_people_int
             elif "75 years and over" in line:
-                income_year_dict["75+"] = line.split("\t")[1:-6]
+                income_year_dict["75+"]   = income_people_int
         return income_year_dict
     
 
@@ -49,17 +59,20 @@ def read_UsData(filename_M, filename_F):
     female_data = read_UsFile(filename_F)
 
     for (age_group,num_m), (age_group,num_f) in zip(male_data.items(), female_data.items()):
-        final_dict[age_group]["M"].append(num_m)
-        final_dict[age_group]["F"].append(num_f) 
+        final_dict[age_group]["M"] = num_m
+        final_dict[age_group]["F"] = num_f 
 
     return final_dict
 
 
-path       = "./data/us/"
+path       = "../../data/us/"
 filename_F = path + "pinc_female.tsv"
 filename_M = path + "pinc_male.tsv"
 
-print(read_UsData(filename_M,filename_F))
+data = read_UsData(filename_M,filename_F)
+
+for key in data:
+    print(f"{key}\n{data[key]}")
 # calculate gini coefficient
 
 # plot lorenz curve
