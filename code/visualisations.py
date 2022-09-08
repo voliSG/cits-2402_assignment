122e260c9303d0cf918b9c9e40def3596d0e1785
Sean <2003seangarcia@gmail.com>
2022-09-08 12:10:47 +0800

    planning

---
+++ code/visualisations.py
@@ -0,0 +1,38 @@
+"""
+  #DATA SCHEMA
+{
+  "15_24": {"M"  : (330462, 134491...),
+            "F"   : (330462, 134491...)},
+  "25_34": ,
+  "35_44": ,
+  "45_54": ,
+  "55_64": ,
+  "65_74": ,
+  "75+"  : ,
+  "All"  : ,
+}
+
+
+##PIE CHART + sex ratio?
+  # sex by country
+    aus_data["All"]["M"] + aus_data["All"]["F"]
+     us_data["All"]["M"] +  us_data["All"]["F"]
+  
+  # age by country
+    - loop through dict and get sex ratio
+    - add new section to chart with 
+      - aus_data[age]["M"] + aus_data[age]["F"]
+
+
+##MEDIAN INCOME BY AGE BY SEX
+  - estimate median income
+
+##LORENZ CURVE
+  # Aus vs US
+
+  # sex by country
+  
+##BAR CHART - RANK GINI COEFFICIENTS BY AGE 
+
+## SEX RATIO BY INCOME QUINTILE BY AGE BY COUNTRY
+"""
