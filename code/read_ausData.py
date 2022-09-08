a82115e24ec9ce8efdd217495e7d1426d9bbd680
unknown <23225186@student.uwa.edu.au>
2022-09-08 19:48:07 +0800

    fixed function

--- code/read_ausData.py
+++ code/read_ausData.py
@@ -1,10 +1,10 @@
 # Data transformation and cleaning
 #path from notebook
-path_dataFolder = "./data/"
+path_dataFolder = "./data/aus/"
 filenames_aus = ["2021Census_G17A_AUS_AUS.csv", "2021Census_G17B_AUS_AUS.csv", "2021Census_G17C_AUS_AUS.csv"]
 
-def read_AusData(path, filenames):
-    """performs data transformation and cleaning for ABS Census data
+#def read_AusData(path, filenames):
+"""performs data transformation and cleaning for ABS Census data
         (G17 TOTAL PERSONAL INCOME (WEEKLY) BY AGE BY SEX)
 
     Args:
@@ -15,7 +15,7 @@ def read_AusData(path, filenames):
     """
 
     # open file
-    """
+"""
     with open(file_1,'r') as f1, open(file_2, 'r') as f2:
         f1= f1.read().splitlines()
         f2=f2.read().splitlines()
@@ -89,5 +89,97 @@ def read_AusData(path, filenames):
         return AUS
     """
       
+def read_AusData(path, filenames):
+    #comibining path to filename creating directory
+    file_list=[]
+    for file in filenames:
+        file_list.append(path+file)
+    
+    #open file and sort between heading and number in each sex
+    heading_M, number_M, heading_F, number_F, heading_P, number_P = [], [], [], [], [], []    
+    for file in file_list:
+        with open(file, 'r') as data:
+            data = data.read()
+            line = data.splitlines()
+            head = line[0].split(',')[1:]
+            num = line[1].split(',')[1:]
+            for i in range(len(head)):
+                # male
+                if head[i].startswith('M'):
+                    heading_M.append(head[i])
+                    number_M.append(int(num[i]))
+                # female
+                if head[i].startswith('F'):
+                    heading_F.append(head[i])
+                    number_F.append(int(num[i]))
+                # population
+                if head[i].startswith('P'):
+                    heading_P.append(head[i])
+                    number_P.append(int(num[i]))
+    
+    #generating dictionary for AUS
+    AUS={key: None for key in ['15_24', '25_34', '35_44', '45_54', '55_64', '65_74', '75+', 'all']}
+    
+    #25-34
+    age_25_34_M = number_M[2::10][:15] # Male 
+    age_25_34_F = number_F[2::10][:15] # Female 
+    age_25_34_P = number_P[2::10][:15] # Population
+    AUS['25_34']={'M':age_25_34_M, 'F':age_25_34_F, 'P':age_25_34_P}
+    
+    #35-44
+    age_35_44_M = number_M[3::10][:15] # Male 
+    age_35_44_F = number_F[3::10][:15] # Female 
+    age_35_44_P = number_P[3::10][:15] # Population
+    AUS['35_44']={'M':age_35_44_M, 'F':age_35_44_F, 'P':age_35_44_P}
+    
+    #45-54
+    age_45_54_M = number_M[4::10][:15] # Male 
+    age_45_54_F = number_F[4::10][:15] # Female 
+    age_45_54_P = number_P[4::10][:15] # Population
+    AUS['45_54']={'M':age_45_54_M, 'F':age_45_54_F, 'P':age_45_54_P}
+    
+    #55-64
+    age_55_64_M = number_M[5::10][:15] # Male 
+    age_55_64_F = number_F[5::10][:15] # Female
+    age_55_64_P = number_P[5::10][:15] # Population
+    AUS['55_64']={'M':age_55_64_M, 'F':age_55_64_F, 'P':age_55_64_P}
+    
+    #65-74
+    age_65_74_M = number_M[6::10][:15] # Male 
+    age_65_74_F = number_F[6::10][:15] # Female 
+    age_65_74_P = number_P[6::10][:15] # Population
+    AUS['65_74']={'M':age_65_74_M, 'F':age_65_74_F, 'P':age_65_74_P}
+    
+    #total
+    age_all_M = number_M[9::10][:15] # Male 
+    age_all_F = number_F[9::10][:15] # Female 
+    age_all_P = number_P[9::10][:15] # Population
+    AUS['all']={'M':age_all_M, 'F':age_all_F, 'P':age_all_P}
+    
+     # combining the data to complete the age group [ (15-19 + 20 -24) and (75-84 + 85&over)]
+    age_15_24_M = number_M[::10] + number_M[1::10]
+    age_15_24_F = number_F[::10] + number_F[1::10]
+    age_15_24_P = number_P[::10] + number_P[1::10]
+    age_75_M = number_M[7::10] + number_M[8::10]
+    age_75_F = number_F[7::10] + number_F[8::10]
+    age_75_P = number_P[7::10] + number_P[8::10]
+    
+    age_15_24_M_f, age_15_24_F_f, age_15_24_P_f = [], [], []
+    age_75_M_f, age_75_F_f, age_75_P_f = [], [], []
+    
+    for i in range(len(age_15_24_M)//2-2):
+        age_15_24_M_f.append(age_15_24_M[i]+age_15_24_M[i+17]) # male 15-24
+        age_15_24_F_f.append(age_15_24_F[i]+age_15_24_F[i+17]) # female 15-24
+        age_15_24_P_f.append(age_15_24_P[i]+age_15_24_P[i+17]) # population 15-24
+        age_75_M_f.append(age_75_M[i]+age_75_M[i+17]) # male 75 over
+        age_75_F_f.append(age_75_F[i]+age_75_F[i+17]) # female 75 over
+        age_75_P_f.append(age_75_P[i]+age_75_P[i+17]) # population 75 over
+    
+    # 15-24
+    AUS['15_24']={'M':age_15_24_M_f, 'F':age_15_24_F_f, 'P':age_15_24_P_f} # append to dict
+    # 75 over
+    AUS['75+']={'M':age_75_M_f, 'F':age_75_F_f, 'P':age_75_P_f} # append to dict
+    
+    return AUS
 
-read_AusData(path, filenames_aus)    
+print(read_AusData(path_dataFolder, filenames_aus))    
